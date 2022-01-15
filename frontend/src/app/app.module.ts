e32882bf24dc58cb8cfa94cca9178f149e53edc7
Borcze Angelov <Borcze.Angelov@yahoo.com>
2022-01-15 22:59:42 +0100

    add some frontend code

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,14 +8,22 @@ import { MessageBoardComponent } from './message-board/message-board.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   declarations: [
     AppComponent,
